06fc55ba0f9a4528938ee96d598c23483e6b4de0
USER <USER@DESKTOP-5N1T23M>
2023-11-03 10:56:53 +0530

    Updated And Created A New Class

--- src/firstclass/TC_002.java
+++ src/firstclass/TC_002.java
@@ -3,8 +3,8 @@ package firstclass;
 public class TC_002 {
 
 	public static void main(String[] args) {
-System.out.println("krishna");
-System.out.println("Bala");
+System.out.println("kumar");
+System.out.println("sai");
 	}
 
 }
